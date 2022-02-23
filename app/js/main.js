b490b038de06b926efe3560112fd75b600e3cbb1
dima <dima.xipkov@gmail.com>
2022-02-23 19:22:41 +0200

    10

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,13 @@
 $(function name(params) {
 
     const swiper = new Swiper('.swiper', {
-        slidesPerView: 1,
-        loop: true,
-        loopedslides: 1,
+        // slidesPerView: 1,
+        // loop: true,
+        // loopedslides: 1,
+        // slidesPerView: 2,
+        // grid: {
+        //     rows: 2,
+        // },
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
@@ -18,21 +22,57 @@ $(function name(params) {
         $('.vacancies__list').slideToggle();
     });
 
-    // $('.product-detalis__input').styler();
 
 
+    $(".scroller").mCustomScrollbar({
+        theme: "dark-2"
+    });
 
-    
-  
-        // (function ($) {
-        //     $(window).on("load", function () {
-        //         $(".div").mCustomScrollbar();
-        //     });
-        // })(jQuery); 
+    // var $header = $(".header")
+    // var scroll = 0
+    // var active = "active"
+    // $(window).scroll(function () {
+    //     if ($(window).scrollTop() > scroll) {
+    //         $header.addClass(active)
+    //     } else {
+    //         $header.removeClass(active)
+    //     }
+    // })
 
-        $(".scroller").mCustomScrollbar({
-            theme: "dark-2"
+    $(function () {
+        let header = $('.menu');
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 1) {
+                header.addClass('header-fixed');
+            } else {
+                header.removeClass('header-fixed');
+            }
         });
+    });
+
+    $(function () {
+
+        (function quantityProducts() {
+            var $quantityArrowMinus = $(".quantity-arrow-minus");
+            var $quantityArrowPlus = $(".quantity-arrow-plus");
+            var $quantityNum = $(".quantity-num");
+
+            $quantityArrowMinus.click(quantityMinus);
+            $quantityArrowPlus.click(quantityPlus);
+
+            function quantityMinus() {
+                if ($quantityNum.val() > 1) {
+                    $quantityNum.val(+$quantityNum.val() - 1);
+                }
+            }
+
+            function quantityPlus() {
+                $quantityNum.val(+$quantityNum.val() + 1);
+            }
+        })();
+
+    });
 
 
     var mixer = mixitup('.slider-content');
@@ -86,66 +126,6 @@ $(function name(params) {
            
            */
 
-    // function getAlignedText(text) {
-    //     text = text.split('\n');
-    //     while (text.length > 0 && $.trim(text[0]) == '') {
-    //         text.shift();
-    //     }
-    //     var tabs = (text[0] || '').replace(/^(\s+).+$/, '$1');
-    //     for (var i = 0; i < text.length; i++) {
-    //         text[i] = text[i].replace(tabs, '');
-    //     }
-    //     if (text.length > 0 && text[text.length - 1].match(/^\s*$/)) {
-    //         text.pop();
-    //     }
-    //     return text.join('\n');
-    // }
 
-    // $('.container').each(function () {
-
-    //     var content = $(this).find('.content');
-    //     var controls = $(this).find('.controls');
-
-    //     $('<pre></pre>').addClass('prettyprint linenums lang-html').text(getAlignedText(content.find('.demo').html())).appendTo(content.find('.html'));
-    //     $('<pre></pre>').addClass('prettyprint linenums lang-css').text(getAlignedText($('#css-common').html()) + "\n" + getAlignedText(content.find('style').html())).appendTo(content.find('.css'));
-    //     $('<pre></pre>').addClass('prettyprint linenums lang-js').text(getAlignedText(content.find('script').html())).appendTo(content.find('.js'));
-
-    //     controls.on('click', 'span', function () {
-    //         content.find('.' + $(this).removeClass('active').attr('class')).show().siblings('div').hide();
-    //         $(this).addClass('active').siblings('span').removeClass('active');
-    //     });
-    //     controls.find('.demo').click();
-    // });
-
-    // $('.container').on('click', '.add-content', function () {
-    //     $('#lorem-ipsum').clone().removeAttr('id').appendTo($(this).closest('.container').find('.scroll-content'));
-    //     return false;
-    // });
-    // $('.container').on('click', '.remove-content', function () {
-    //     $(this).closest('.container').find('.scroll-content').find('p').not('.permanent').last().remove();
-    //     return false;
-    // });
-
-    // window.prettyPrint && prettyPrint();
-    // $('.wrapper').scrollbar();
-
-
-    // const demo = document.querySelector('.demo1 .demo');
-    // const ps = new PerfectScrollbar(demo);
-
-    // // Handle size change
-    // document.querySelector('#resize').addEventListener('click', () => {
-
-    //     // Get updated values
-    //     // width = document.querySelector('#width').value;
-    //     height = document.querySelector('#height').value;
-
-    //     // Set demo sizes
-    //     // demo.style.width = `${width}px`;
-    //     demo.style.height = `${height}px`;
-
-    //     // Update Perfect Scrollbar
-    //     ps.update();
-    // });
 
 });
